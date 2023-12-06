6fe72d8b31ec423cc80e0af9e9f6a2c24621c31b
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-12-07 07:21:27 +0900

    Update main.py

--- Stub_FAPI/main.py
+++ Stub_FAPI/main.py
@@ -1,18 +1,19 @@
 from .model.db import Base
-# create engine
 import uvicorn
 from sqlalchemy import create_engine
 
 from .app import app
 
-# create tables
 def main():
-    engine = create_engine("sqlite:///./sql_app.db", echo=True)
+    # init------------------------------------------------------------------
+    engine = create_engine("sqlite:///./sql_app.db", echo=True) # エンジン作成
     Base.metadata.create_all(bind=engine) # テーブル作成
-
     engine.dispose() # エンジン破棄
-    
-    uvicorn.run(app, host="0.0.0.0", port=8000) # FastAPIサーバー起動
+    #-----------------------------------------------------------------------
+
+    # uvicornでFastAPIサーバーを起動
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # 補足: 0.0.0.0は全アドレスをListenする
 
 
 
